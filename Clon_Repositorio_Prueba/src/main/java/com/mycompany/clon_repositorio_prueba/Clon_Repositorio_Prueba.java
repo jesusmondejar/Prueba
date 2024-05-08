2f11f2f508df7273292eccf7d040361bd4658e74
34722 <34722@10.81.106.67>
2024-05-08 09:46:41 +0200

    Subir los archivos a GitHub

---
+++ Clon_Repositorio_Prueba/src/main/java/com/mycompany/clon_repositorio_prueba/Clon_Repositorio_Prueba.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.clon_repositorio_prueba;
+
+/**
+ *
+ * @author 34722
+ */
+public class Clon_Repositorio_Prueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Clon_Repositorio_Prueba/src/main/java/com/mycompany/clon_repositorio_prueba/Videojuego.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.clon_repositorio_prueba;
+
+/**
+ *
+ * @author 34722
+ */
+public class Videojuego {
+    
+}
